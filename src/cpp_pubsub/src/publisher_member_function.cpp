78dfe5d12d276389f74feddf30c85f7f079a39bb
erickchicatto1 <robo.12@outlook.es>
2026-05-19 15:55:02 -0600

    first commit

---
+++ src/cpp_pubsub/src/publisher_member_function.cpp
@@ -0,0 +1,29 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+using namespace std::chrono_literals;
+
+class MinimalPublisher: public rclcpp::Node
+{
+    
+   
+ 
+
+
+
+
+
+};
+
+
+
+int main(){
+
+
+   return 0;
+}
