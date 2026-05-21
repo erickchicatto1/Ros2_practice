ab5be00c203665dc623c1ccfe2080b9fed9fa860
erickchicatto1 <robo.12@outlook.es>
2026-05-21 15:53:34 -0600

    adding Interfaces

--- src/my_robot_pkg/my_robot_pkg/subscriber_member_function.py
+++ src/my_robot_pkg/my_robot_pkg/subscriber_member_function.py
@@ -1,22 +1,21 @@
 import rclpy
-from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from tutorial_interfaces.msg import Num
 
 class MinimalSubscriber(Node):
     
     def __init__(self):
         super().__init__('minimal_publisher')
         self.subscription = self.create_subscription(
-	    String,
+	    Num,
 	    'topic',
 	    self.listener_callback,
 	    10)
         self.subscription
 	
     def listener_callback(self,msg):
-        self.get_logger().info('I heard:"%s"' % msg.data)
+        self.get_logger().info('I heard:"%s"' % msg.num)
         
         
 def main(args=None):
